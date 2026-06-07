e672b81b4bc3f64885873113722ff749815a867c
mustafaorhan-dev <mustafa.orhan@ahievran.edu.tr>
2026-06-07 04:02:52 +0300

    DD

--- app.js
+++ app.js
@@ -1296,7 +1296,7 @@ function refreshDashboard() {
   const qiContainer = document.getElementById('quick-info-list');
   qiContainer.innerHTML = `
     <div style="display:flex;flex-direction:column;">
-      <div><canvas id="quick-info-canvas" style="width:100%;"></canvas></div>
+      <div style="max-width:300px;margin:0 auto;"><canvas id="quick-info-canvas" style="width:100%;"></canvas></div>
       <div style="display:flex;justify-content:center;gap:14px;flex-wrap:wrap;" id="qi-legend"></div>
     </div>
   `;
